aaef6905c27d1b08cae496b091bfa20735aded10
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-12 14:52:59 +0200

    Update letIsLambda.scm

--- letIsLambda.scm
+++ letIsLambda.scm
@@ -7,3 +7,25 @@
 
 ((lambda (foo bar baz)
    (+ foo bar baz)) 1 2 3)
+
+
+(define (foo-1 x)
+  (let ((two-x (+ x x)))
+    (+ x two-x)))
+
+;; is just a nicer way of writing 
+  
+(define (foo-2 x)
+  ((lambda (two-x)
+     (+ x two-x)) 
+   (+ x x)))
+
+;; which in turn is just syntactic sugar for:
+
+(define foo-3
+  (lambda (x)
+    ((lambda (two-x)
+       (+ x two-x)) 
+     (+ x x))))
+
+;; foo-1, foo-2 and foo-3 all do EXACTLY the same thing
