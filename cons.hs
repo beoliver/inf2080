0be3ed57cf076569f25270427123c3f5aafcdeb0
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-14 22:09:53 +0200

    Create cons.hs

---
+++ cons.hs
@@ -0,0 +1,19 @@
+cons :: a -> b -> (a -> b -> c) -> c
+cons a b selector = selector a b
+
+car :: ((a -> b -> a) -> c) -> c
+car ab = ab (\a b -> a)
+-- car ab = ab const
+
+cdr :: ((a -> b -> b) -> c) -> c
+cdr ab = ab (\a b -> b)
+-- cdr ab = ab (flip const)
+
+cadr :: ((a -> b -> b) -> (c -> d -> c) -> e) -> e
+cadr = car . cdr
+
+xs = cons (Just "the power of closures") $ 
+     cons "a" $ 
+     cons 5.6 $ 
+     cons 1 $ 
+     cons 'a' []
