f27c355ccc20e41dcf6ea63bb78932c964bdced5
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-14 18:09:17 +0200

    Update evaluation.scm

--- evaluation.scm
+++ evaluation.scm
@@ -12,15 +12,28 @@
 ;; => 6
 
 ;; evaluates from left to right
+;; for every expression that is passed as an argument
+;; it is evaluated and then then passed to the procedure 'list
+;; if every arg was evaluated before calling 'list
+;; then the result would be '(1 1 1)
+
 
 (define x 1)
 
 (list x 
-  (begin 
-    (set! x (+ x x)) 
-    x) 
-  (begin 
-    (set! x (- x 1)) 
-    x))
-    
+      (begin (set! x (+ x x)) x) 
+      (begin (set! x (- x 1)) x))
+
+;; => (1 2 1)
+
+(define scheme-list
+  (lambda args
+    (if (null? args)
+	  '()
+	  (cons (car args) (apply scheme-list (cdr args))))))
+
+(scheme-list x 
+	     (begin (set! x (+ x x)) x) 
+	     (begin (set! x (- x 1)) x))
+
 ;; => (1 2 1)
