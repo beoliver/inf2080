4f0c92b395edbc69c91c6d85475ec75e80739385
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-14 22:16:22 +0200

    Update cons.hs

--- cons.hs
+++ cons.hs
@@ -1,12 +1,15 @@
 cons :: a -> b -> (a -> b -> c) -> c
 cons a b selector = selector a b
+-- cons = \ a b f -> f a b
 
 car :: ((a -> b -> a) -> c) -> c
 car ab = ab (\a b -> a)
+-- car = \ ab -> ab $ \ a b -> a
 -- car ab = ab const
 
 cdr :: ((a -> b -> b) -> c) -> c
 cdr ab = ab (\a b -> b)
+-- cdr = \ ab -> ab $ \ a b -> b
 -- cdr ab = ab (flip const)
 
 cadr :: ((a -> b -> b) -> (c -> d -> c) -> e) -> e
