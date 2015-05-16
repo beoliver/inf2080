cb46c11eec5be5f8f0177efde1d0822aea5fd86a
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-16 19:16:12 +0200

    Update basicProces.scm

--- basicProces.scm
+++ basicProces.scm
@@ -10,4 +10,10 @@
           frames)))))
 
 
+(define update-local-bindings! ;; #{Unspecific} return value
+  (lambda (key-val frames)
+    (let ((pair (assoc (car key-val) (car frames))))
+      (if pair
+	      (set-cdr! pair (cdr key-val))
+	      (set-car! frames (cons key-val (car frames)))))))
 
