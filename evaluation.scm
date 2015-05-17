768e802ebccbb95c9a3490eabaf495ac94c76bb2
Benjamin Edward Oliver <benjamin.e.oliver@gmail.com>
2015-05-17 14:00:48 +0200

    Update evaluation.scm

--- evaluation.scm
+++ evaluation.scm
@@ -37,3 +37,38 @@
 	     (begin (set! x (- x 1)) x))
 
 ;; => (1 2 1)
+
+
+;; some "strange" results using scheme48
+
+(define (f x)
+    (lambda () x))
+
+(let ((g (f x)))
+        (set! x 0)
+        (g))
+
+;; REPL
+;  1
+;> x
+;  0
+
+(define xs (list 1 2 3))
+; no values returned
+(let ((g (f xs)))
+        (set-car! xs 0)
+        (g))
+
+;; REPL
+;  (0 2 3) 
+; note that 1 is now 0
+
+
+(define ys '(1 2 3))
+; no values returned
+(let ((g (f ys)))
+        (set-car! ys 0)
+        (g))
+
+assertion-violation: immutable argument [set-car!]
+                     (set-car! '(1 2 3) 0)
